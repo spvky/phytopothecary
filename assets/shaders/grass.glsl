feff26c479b584f1136f0525095c13c52dc936be
spooky-th-ghost <colin@wildheartinteractive.com>
2025-03-16 18:44:07 -0400

    player responsive shader working

--- assets/shaders/grass.glsl
+++ assets/shaders/grass.glsl
@@ -9,6 +9,7 @@ in vec4 vertexColor;
 // Input uniform values
 uniform mat4 mvp;
 uniform float time;
+uniform vec3 playerPos;
 uniform sampler2D noise;
 uniform sampler2D check;
 
@@ -18,11 +19,18 @@ out vec4 fragColor;
 
 void main()
 {
+		bool near_player = distance(vertexPosition, playerPos) < 1.0;
 		float noise_value = texture2D(noise, vertexTexCoord).r;
 		//float height = sin(time * (noise_value * 10.0)) * 0.5;
-		float height = noise_value + (sin(time) * 0.02);
+		float height  = noise_value + (sin(time) * 0.02);
 		float sway = height * sin(time);
 		vec3 newPosition = vec3(vertexPosition.x + sway, height, vertexPosition.z);
+		if (near_player) {
+			vec3 target_direction = normalize(vertexPosition - playerPos);
+			vec3 new_poinst = playerPos + target_direction;
+			newPosition = new_poinst;
+		}
+
 		
 
     fragTexCoord = vertexTexCoord;
--- src/main.zig
+++ src/main.zig
@@ -6,6 +6,16 @@ const World = @import("world.zig").World;
 
 pub const Player = struct {};
 
+pub fn player_shader_stuff(world: *World, shader: rl.Shader) void {
+    const loc = rl.getShaderLocation(shader, "playerPos");
+    var view = world.registry.view(.{ physics.Transform, Player }, .{});
+    var iter = view.entityIterator();
+
+    while (iter.next()) |entity| {
+        const transform = view.getConst(physics.Transform, entity);
+        rl.setShaderValue(shader, loc, &transform.translation, .vec3);
+    }
+}
 pub fn spawn_balls(world: *World) void {
     const t1: physics.Transform = .{ .translation = .{ .x = 2, .y = 2, .z = 0 } };
     const t2: physics.Transform = .{ .translation = .{ .x = 1, .y = 2, .z = 0 } };
@@ -14,15 +24,14 @@ pub fn spawn_balls(world: *World) void {
 }
 
 pub fn update_balls(world: *World, delta: f32) void {
-    var view = world.registry.view(.{physics.Transform}, .{Player});
+    var view = world.registry.view(.{physics.Transform}, .{});
     var iter = view.entityIterator();
 
     while (iter.next()) |entity| {
-        var pos = view.get(physics.Transform, entity);
-        pos.translation.x += 1 * delta;
+        var pos = view.get(entity);
+        pos.translation.z += 0.5 * delta;
     }
 }
-
 pub fn draw_balls(world: *World) void {
     var view = world.registry.view(.{physics.Transform}, .{});
     var iter = view.entityIterator();
@@ -47,12 +56,12 @@ pub fn main() !void {
 
     var model = try rl.loadModel("./assets/models/cheffy.glb");
 
-    const player_id = world.spawn(.{ Player, physics.Transform{ .translation = .{ .x = 0, .y = 0, .z = 0 } }, rendering.Model{ .model = &model } });
+    const player_id = world.spawn(.{ Player, physics.Transform{ .translation = .{ .x = 3, .y = 0, .z = 0 } }, rendering.Model{ .model = &model } });
     _ = player_id;
     const noise = try rl.loadTextureFromImage(rl.genImagePerlinNoise(1024, 1024, 50, 50, 4.0));
     const check = try rl.loadTexture("./assets/textures/check.png");
     const shader = try rl.loadShader("./assets/shaders/grass.glsl", null);
-    const grass_patch = try rl.loadModelFromMesh(rl.genMeshPlane(100, 100, 100, 100));
+    const grass_patch = try rl.loadModelFromMesh(rl.genMeshPlane(10, 10, 100, 100));
 
     const material_count: usize = @intCast(grass_patch.materialCount);
     for (0..material_count) |i| {
@@ -70,12 +79,11 @@ pub fn main() !void {
         update_balls(&world, delta);
         const time: f32 = @floatCast(rl.getTime());
         rl.setShaderValue(shader, time_loc, &time, .float);
+        player_shader_stuff(&world, shader);
         rl.beginDrawing();
         camera.begin();
         rl.clearBackground(rl.Color.dark_gray);
-
         rendering.draw_models(&world);
-        // rl.drawModel(model, .{ .x = 0, .y = y_position, .z = 0 }, 1, rl.Color.white);
         rl.drawModel(grass_patch, .{ .x = 0, .y = -3, .z = 0 }, 1, rl.Color.sky_blue);
         camera.end();
         rl.endDrawing();
