3d4958db3120eb8cc81f5b7a9c0e3918d97b57ca
spooky-th-ghost <colin@wildheartinteractive.com>
2025-03-28 21:59:45 -0400

    road is back in

--- src/main.zig
+++ src/main.zig
@@ -4,27 +4,12 @@ const physics = @import("physics.zig");
 const World = @import("world.zig").World;
 const ModelTag = @import("world.zig").ModelTag;
 const rendering = @import("rendering.zig");
+const player = @import("player.zig");
 
 comptime {
     _ = @import("test.zig");
 }
 
-pub const Player = struct {};
-
-pub fn player_shader_stuff(world: *World) void {
-    const road_shader_opt = world.shaders.get(.road);
-
-    if (road_shader_opt) |shader| {
-        const loc = rl.getShaderLocation(shader, "playerPos");
-        var view = world.registry.view(.{ physics.Transform, Player }, .{});
-        var iter = view.entityIterator();
-
-        while (iter.next()) |entity| {
-            const transform = view.getConst(physics.Transform, entity);
-            rl.setShaderValue(shader, loc, &transform.translation, .vec3);
-        }
-    }
-}
 pub fn main() !void {
     var world = try World.init(std.heap.page_allocator);
     defer world.deinit();
@@ -32,21 +17,21 @@ pub fn main() !void {
     // Load Models
     var grass = try rl.loadModelFromMesh(rl.genMeshPlane(10, 10, 100, 100));
     var road = try rl.loadModelFromMesh(rl.genMeshPlane(15, 100, 250, 250));
-    var player = try rl.loadModel("./assets/models/cheffy.glb");
+    var player_model = try rl.loadModel("./assets/models/cheffy.glb");
 
     var grass_shader = try rl.loadShader("./assets/shaders/grass.glsl", null);
     var road_shader = try rl.loadShader("./assets/shaders/wavy_road.glsl", "./assets/shaders/road_color.glsl");
 
     try world.store_model(.grass, &grass);
     try world.store_model(.road, &road);
-    try world.store_model(.player, &player);
+    try world.store_model(.player, &player_model);
 
     try world.store_shader(.grass, &grass_shader);
     try world.store_shader(.road, &road_shader);
 
     world.map_shaders();
 
-    const player_id = world.spawn(.{ Player, physics.Transform{ .translation = .{ .x = 3, .y = 0, .z = 0 } }, rendering.Model{ .model = &player } });
+    const player_id = world.spawn(.{ player.Player, physics.Transform{ .translation = .{ .x = 3, .y = 0, .z = 0 } }, rendering.Model{ .model = &player_model } });
     _ = player_id;
 
     const road_id = world.spawn(.{ physics.Transform{ .translation = .{ .x = 0, .y = 0, .z = 0 } }, rendering.Model{ .model = &road } });
@@ -61,7 +46,7 @@ pub fn main() !void {
 
     while (!rl.windowShouldClose()) {
         world.update();
-        player_shader_stuff(&world);
+        player.player_shader_update(&world);
         world.draw();
     }
 }
--- src/player.zig
+++ src/player.zig
@@ -1,1 +1,20 @@
+const rl = @import("raylib");
+const World = @import("world.zig").World;
+const physics = @import("physics.zig");
+
 pub const Player = struct {};
+
+pub fn player_shader_update(world: *World) void {
+    const road_shader_opt = world.shaders.get(.road);
+
+    if (road_shader_opt) |shader| {
+        const loc = rl.getShaderLocation(shader, "playerPos");
+        var view = world.registry.view(.{ physics.Transform, Player }, .{});
+        var iter = view.entityIterator();
+
+        while (iter.next()) |entity| {
+            const transform = view.getConst(physics.Transform, entity);
+            rl.setShaderValue(shader, loc, &transform.translation, .vec3);
+        }
+    }
+}
